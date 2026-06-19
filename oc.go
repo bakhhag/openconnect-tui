504d06f03dc5c26686f4d7846c81a9906236c10e
cholem0 <charAnais@proton.me>
2026-06-19 16:17:52 +0330

    feat: separated windows platform logic

--- oc.go
+++ oc.go
@@ -3,15 +3,8 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
-	"os"
 	"os/exec"
-	"runtime"
 	"strings"
-	"syscall"
-
-	"github.com/joho/godotenv"
-	"golang.org/x/sys/windows"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -24,59 +17,9 @@ type SelectedServer struct {
 	Pass string
 }
 
-func amIAdmin() bool {
-	var sid *windows.SID
-
-	err := windows.AllocateAndInitializeSid(
-		&windows.SECURITY_NT_AUTHORITY,
-		2,
-		windows.SECURITY_BUILTIN_DOMAIN_RID,
-		windows.DOMAIN_ALIAS_RID_ADMINS,
-		0, 0, 0, 0, 0, 0,
-		&sid,
-	)
-	if err != nil {
-		return false
-	}
-	defer windows.FreeSid(sid)
-
-	token := windows.Token(0)
-	member, err := token.IsMember(sid)
-	if err != nil {
-		return false
-	}
-	return member
-}
-
-func runAsAdmin() {
-	verb := "runas"
-	exe, err := os.Executable()
-	if err != nil {
-		log.Fatalf("Failed to get executable path: %v", err)
-	}
-	cwd, _ := os.Getwd()
-	args := strings.Join(os.Args[1:], " ")
-
-	verbPtr, _ := syscall.UTF16PtrFromString(verb)
-	exePtr, _ := syscall.UTF16PtrFromString(exe)
-	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
-	argPtr, _ := syscall.UTF16PtrFromString(args)
-
-	var showCmd int32 = 1
-
-	err = windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-	if err != nil {
-		log.Fatalf("Failed to elevate process: %v", err)
-	}
-	os.Exit(0)
-}
-
 func openconnect(p *tea.Program, stopChan <-chan struct{}, doneChan chan<- struct{}, sv Profile, flags []FlagRow) {
 	defer close(doneChan)
-	var err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+
 	var TARGET_SERVER string = fmt.Sprintf("%s:%s", sv.IP, sv.Port)
 	var OC_USER string = sv.User
 	var OC_PASS string = sv.Pass
@@ -150,39 +93,4 @@ func openconnect(p *tea.Program, stopChan <-chan struct{}, doneChan chan<- struc
 	}
 	cmd.Wait()
 	p.Send(vpnStatusMsg("0"))
-}
-
-func configureSysProcAttr(cmd *exec.Cmd) {
-	if runtime.GOOS == "windows" {
-		cmd.SysProcAttr = &syscall.SysProcAttr{
-			CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
-		}
-	}
-}
-
-func interruptProcess(cmd *exec.Cmd) error {
-	if cmd.Process == nil {
-		return fmt.Errorf("process not started")
-	}
-
-	if runtime.GOOS == "windows" {
-		dll, err := syscall.LoadDLL("kernel32.dll")
-		if err != nil {
-			return err
-		}
-		defer dll.Release()
-
-		proc, err := dll.FindProc("GenerateConsoleCtrlEvent")
-		if err != nil {
-			return err
-		}
-
-		r, _, errResult := proc.Call(syscall.CTRL_BREAK_EVENT, uintptr(cmd.Process.Pid))
-		if r == 0 {
-			return errResult
-		}
-		return nil
-	}
-
-	return cmd.Process.Signal(os.Interrupt)
 }
